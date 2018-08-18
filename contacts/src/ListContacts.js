32ac4f222c25c2c393eeb6c6b62b6fb9474194d7
Monica Moura <monica.a.moura@hotmail.com>
2018-08-18 18:27:13 +0100

    Add input field and updateState

--- contacts/src/ListContacts.js
+++ contacts/src/ListContacts.js
@@ -1,38 +1,93 @@
-import React from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
-function ListContacts (props) {
-    return (
-        <ol className='contact-list'>
-            {/*Access contacts in ContactList Component from App.js through props*/}
-            {props.contacts.map((contact) => //map over contacts
-                <li key={contact.id} className='contact-list-item'>
-                    <div className='contact-avatar' style={{
-                        backgroundImage: `url(${contact.avatarURL})`   //url is whatever the contact avatar is
-                    }}/>
-                    <div className='contact-details'>
-                        <p>{contact.name}</p>
-                        <p>{contact.email}</p>
-                    </div>
-                    {/* call props onDeleteContact */}
-                    <button onClick={() => props.onDeleteContact(contact)} className='contact-remove'>
-                        Remove
-                    </button>
-                </li>
-            )}
-        </ol>
-    )
+class ListContacts extends Component {
+    // added PropTypes as a static property on Class
+    static PropTypes = {
+        contacts: PropTypes.array.isRequired,
+        onDeleteContact: PropTypes.func.isRequired
+    }
+
+    state = {
+        query: 
+    }
+
+    //whenever the input field changes we want to update
+    //our query
+    //create an updateQuery method that takes in a new (query)
+    updateQuery = () => {
+        ///update the state
+        //we will not update the state based on previous state so we
+        //can just pass an object and use trim() to remove extra white
+        //space
+        //we want query to be whatever query: query.trim() is
+        this.setState({ query: query.trim() })
+
+    }
+
+
+    render() {
+        return (
+            //in react we can only return ONE element so to add our
+            //input field we need to put all this inside one single div
+            <div className='list-contacts'>
+                <div>
+                    <input className='search-contacts'
+                    type='text'
+                    placeholder='Search contacts'
+                    //we want the value of the query to always be what
+                    //this.state.query is
+                    value={this.state.query}
+                    //after updateQuery, whenever the input field changes
+                    //give us an (event)
+                    onChange={(event) => this.updateQuery(event.target.value)}
+                    >
+                </div>
+                <ol className='contact-list'>
+                    {/*Access contacts in ContactList Component from
+                    App.js through props* <<<<< DEPRECATED SINCE WE REFACTORED TO A
+                    COMPONENT */}
+                    {/* After refactoring into a component, it is not receiving props
+                    as an argument any longer so we need to add this before props
+                    this.props*/}
+                    {this.props.contacts.map((contact) => ( //map over contacts
+                        <li key={contact.id} className='contact-list-item'>
+                            <div className='contact-avatar' style={{
+                                backgroundImage: `url(${contact.avatarURL})`   //url is whatever the contact avatar is
+                            }}/>
+                            <div className='contact-details'>
+                                <p>{contact.name}</p>
+                                <p>{contact.email}</p>
+                            </div>
+                            {/* call props onDeleteContact */}
+                            <button onClick={() => this.props.onDeleteContact(contact)} className='contact-remove'>
+                            Remove
+                            </button>
+                        </li>
+                    ))}
+                </ol>
+            </div>
+        )
+    }
 }
+
+
+/* >>>>>>>> PROP TYPES <<<<<<<< */
 
 // it will have 2 properties on this ListContacts:
 // contacts and onDeleteContact
 // and we will define the value of each propertie key's type
 // this allows to specify the types of the props we pass on to a
 // component and also allows us to specify if they are required or not
-ListContacts.PropTypes = {
+
+//to add input field, we converted stateless functional app into react
+//component again in order to have a state, and moved PropTypes up as a
+// static prop of the ListContacts class
+
+/*ListContacts.PropTypes = {
     contacts: PropTypes.array.isRequired,
     onDeleteContact: PropTypes.func.isRequired
-}
+}*/
 
 
 export default ListContacts
