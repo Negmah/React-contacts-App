c8c73947dbfad8e75e3d6a703add764d9dc017ec
Monica Moura <monica.a.moura@hotmail.com>
2018-08-18 12:13:39 +0100

    Refactor Component into stateless functional component

--- contacts/src/ListContacts.js
+++ contacts/src/ListContacts.js
@@ -1,27 +1,25 @@
 import React , { Component } from 'react';
 
-class ListContacts extends Component {
-    render () {
-        return (
-            <ol className='contact-list'>
-                {/*Access contacts in ContactList Component from App.js through props*/}
-                {this.props.contacts.map((contact) => //map over contacts
-                    <li key={contact.id} className='contact-list-item'>
-                        <div className='contact-avatar' style={{
-                            backgroundImage: `url(${contact.avatarURL})`   //url is whatever the contact avatar is
-                        }}/>
-                        <div className='contact-details'>
-                            <p>{contact.name}</p>
-                            <p>{contact.email}</p>
-                        </div>
-                        <button className='contact-remove'>
-                            Remove
-                        </button>
-                    </li>
-                )}
-            </ol>
-        )
-    }
+function ListContacts (props) {
+    return (
+        <ol className='contact-list'>
+            {/*Access contacts in ContactList Component from App.js through props*/}
+            {props.contacts.map((contact) => //map over contacts
+                <li key={contact.id} className='contact-list-item'>
+                    <div className='contact-avatar' style={{
+                        backgroundImage: `url(${contact.avatarURL})`   //url is whatever the contact avatar is
+                    }}/>
+                    <div className='contact-details'>
+                        <p>{contact.name}</p>
+                        <p>{contact.email}</p>
+                    </div>
+                    <button className='contact-remove'>
+                        Remove
+                    </button>
+                </li>
+            )}
+        </ol>
+    )
 }
 
 
